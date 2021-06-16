a124f063366e1ef28dacec40dab947cb0c60d9a8
Tucker Barrett <tuckerbarrett@gmail.com>
2021-06-15 23:45:45 -0400

    Adds final images and makes substantial edits to html and css

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-### Personal Portfolio Page
+# Personal Portfolio Page
 
 This repository is for my personal portfolio website, which I am in the beginning stages of creating. This site will be referenced on all job applications and my resume. It is intended to highlight my web development skills on its own, as well as to showcase other notable programming accomplishments and projects. I'm excited to make this my best web project yet!
+
+## Screenshot of completed application:
+![Screenshot of Tucker's completed Portfolio Page](images/placeholder.png "Tucker's completed Horiseon")
+
+## Link to deployed application:
+[Tucker Barrett's Horiseon](http://grinninbarrett.github.io/personal-portfolio-page "Tucker's completed week 2 homework")
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,44 @@
 let mobileNavButton = document.querySelector("#mobile-nav");
 let mobileNavMenu = document.querySelector("#nav-menu");
+let topProject = document.querySelector("#top-project-container");
+let largeScreenOthers = document.querySelector("#projects-container");
+let allProjects = document.querySelectorAll(".projects");
+let wrapper = document.createElement("div");
+let small = false;
 
 
 mobileNavButton.addEventListener("click", () => {
     mobileNavMenu.classList.toggle("menu-shown");
     mobileNavMenu.style.display = "flex";
 });
+
+window.onresize = function() {
+    if (window.innerWidth < 768) {
+        small = true;
+        topProject.outerHTML = topProject.innerHTML;
+        largeScreenOthers.outerHTML = largeScreenOthers.innerHTML;
+        // insert wrapper before el in the DOM tree
+        allProjects.parentNode.insertBefore(wrapper, allProjects);
+        // move el into wrapper
+        wrapper.appendChild(allProjects);
+        wrapper.setAttribute("id", "projects-container");
+        // topProject.classList.remove("top-project-container");
+        // topProject.classList.add("projects-container");
+        // largeScreenOthers.removeAttribute("id");
+        // largeScreenOthers.classList.add("projects-container")
+    }
+};
+
+
+
+// function unwrap(wrapper) {
+//     // place childNodes in document fragment
+//     var docFrag = document.createDocumentFragment();
+//     while (wrapper.firstChild) {
+//         var child = wrapper.removeChild(wrapper.firstChild);
+//         docFrag.appendChild(child);
+//     }
+
+//     // replace wrapper with document fragment
+//     wrapper.parentNode.replaceChild(docFrag, wrapper);
+// }
