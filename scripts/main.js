f9cd93aa8eb896f22a1b9244dc5d1423ecfd089f
Tucker Barrett <tuckerbarrett@gmail.com>
2021-06-19 15:42:14 -0400

    Fixes mobile hamburger menu and changes desktop nav design

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
-let mobileNavButton = document.querySelector("#mobile-nav");
+let mobileNavButton = document.querySelector(".fas");
 let mobileNavMenu = document.querySelector("#nav-menu");
+let mobileNavRoot = document.querySelector("#mobile-nav-menu-root");
 
 
 mobileNavButton.addEventListener("click", () => {
+    mobileNavButton.classList.toggle("fa-times");
+    mobileNavButton.classList.toggle("fa-bars");
     mobileNavMenu.classList.toggle("menu-shown");
-    mobileNavMenu.style.display = "flex";
 });
